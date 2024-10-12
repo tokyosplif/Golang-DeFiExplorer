02c9adb839cf16d268432b3dd210a5f4373037c3
oleg <olegsapozhnik6699@gmail.com>
2024-10-12 22:30:04 +0300

    update address

--- internal/models/address.go
+++ internal/models/address.go
@@ -1,7 +1,10 @@
 package models
 
+import "time"
+
 type Address struct {
-	Id       int64
-	Address  string
-	WalletId int64
+	Id        int64
+	Address   string
+	WalletId  int64
+	CreatedAt time.Time
 }
