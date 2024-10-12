8b30497ee80172ac62aed85d613eecfa65da6422
oleg <olegsapozhnik6699@gmail.com>
2024-10-12 22:28:00 +0300

    update server

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,1 +1,61 @@
 package server
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func NewServer(addr string, handler http.Handler) *Server {
+	return &Server{
+		httpServer: &http.Server{
+			Addr:    addr,
+			Handler: handler,
+		},
+	}
+}
+
+func (s *Server) Start() error {
+	log.Printf("Запуск сервера на %s...", s.httpServer.Addr)
+	if err := s.httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		return fmt.Errorf("ошибка при запуске сервера: %v", err)
+	}
+	return nil
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	log.Println("Завершение работы сервера...")
+	if err := s.httpServer.Shutdown(ctx); err != nil {
+		return fmt.Errorf("ошибка при завершении работы сервера: %v", err)
+	}
+	log.Println("Сервер успешно завершен")
+	return nil
+}
+
+func (s *Server) Run() {
+	go func() {
+		if err := s.Start(); err != nil {
+			log.Fatalf("Ошибка сервера: %v", err)
+		}
+	}()
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	<-ch
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatalf("Ошибка при завершении работы: %v", err)
+	}
+}
