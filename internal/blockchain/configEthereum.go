a12065c9880caa728c557b0aa46dfda3e53d428a
oleg <olegsapozhnik6699@gmail.com>
2024-10-12 22:30:46 +0300

    update configEth

--- internal/blockchain/configEthereum.go
+++ internal/blockchain/configEthereum.go
@@ -1,1 +1,29 @@
 package blockchain
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type EthereumConfig struct {
+	RPCURL string // URL для подключения к Ethereum RPC
+}
+
+func LoadConfig() (*EthereumConfig, error) {
+	if err := godotenv.Load(); err != nil {
+		log.Printf("Ошибка при загрузке .env файла: %v", err)
+		return nil, err
+	}
+
+	rpcURL := os.Getenv("ETHEREUM_RPC_URL")
+	if rpcURL == "" {
+		log.Println("Предупреждение: переменная окружения ETHEREUM_RPC_URL не установлена")
+		return nil, nil
+	}
+
+	return &EthereumConfig{
+		RPCURL: rpcURL,
+	}, nil
+}
