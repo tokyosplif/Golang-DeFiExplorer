f884c351f0c7786e61de770677bab756df193e84
oleg <olegsapozhnik6699@gmail.com>
2024-10-12 22:28:15 +0300

    update router

--- cmd/server/router.go
+++ cmd/server/router.go
@@ -1,1 +1,19 @@
 package server
+
+import (
+	"Golang-DeFiExplorer/internal/blockchain"
+	"Golang-DeFiExplorer/internal/handlers"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+func NewRouter(indexer *blockchain.Indexer) http.Handler {
+	r := chi.NewRouter()
+
+	r.Post("/user", handlers.CreateUser)
+	r.Post("/transaction", handlers.CreateTransaction)
+	r.Get("/block/{id}", handlers.GetBlock(indexer))
+	r.Get("/wallet/{id}", handlers.GetWallet)
+
+	return r
+}
